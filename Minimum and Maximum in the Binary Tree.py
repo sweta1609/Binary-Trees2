54e892c16a5f18b1948ab3d7247095414e9dff42
sweta <52633654+sweta1609@users.noreply.github.com>
2022-03-12 20:57:01 +0530

    Create Minimum and Maximum in the Binary Tree.py

---
+++ Minimum and Maximum in the Binary Tree.py
@@ -0,0 +1,117 @@
+from sys import stdin, setrecursionlimit
+import queue
+
+setrecursionlimit(10 ** 6)
+
+
+#Following is the structure used to represent the Binary Tree Node
+class BinaryTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+
+#Representation of the Pair Class
+class Pair :
+
+    def __init__(self, minimum, maximum) :
+        self.minimum = minimum
+        self.maximum = maximum
+
+INT_MIN = -2147483648
+INT_MAX = 2147483647
+
+
+def findmax(root):
+    if root==None:
+        return INT_MIN
+    
+    leftmax=findmax(root.left)
+    rightmax=findmax(root.right)
+    maxval=max(root.data,max(leftmax,rightmax))
+    return maxval
+def findmin(root):
+    if root==None:
+        return INT_MAX
+    
+    leftmin=findmin(root.left)
+    rightmin=findmin(root.right)
+    minval=min(root.data,min(leftmin,rightmin))
+    return minval
+    
+
+def getMinAndMax(root) :
+    #Your code goes here
+    minvalue=findmin(root)
+    maxvalue=findmax(root)
+    return Pair(minvalue,maxvalue)
+
+#Taking level-order input using fast I/O method
+def takeInput():
+    levelOrder = list(map(int, stdin.readline().strip().split(" ")))
+    start = 0
+    
+    length = len(levelOrder)
+
+    if length == 1 :
+        return None
+    
+    root = BinaryTreeNode(levelOrder[start])
+    start += 1
+
+    q = queue.Queue()
+    q.put(root)
+
+    while not q.empty():
+        currentNode = q.get()
+
+        leftChild = levelOrder[start]
+        start += 1
+
+        if leftChild != -1:
+            leftNode = BinaryTreeNode(leftChild)
+            currentNode.left =leftNode
+            q.put(leftNode)
+
+        rightChild = levelOrder[start]
+        start += 1
+
+        if rightChild != -1:
+            rightNode = BinaryTreeNode(rightChild)
+            currentNode.right =rightNode
+            q.put(rightNode)
+
+    return root
+
+    
+def printLevelWise(root):
+    if root is None:
+        return
+
+    inputQ = queue.Queue()
+    outputQ = queue.Queue()
+    inputQ.put(root)
+
+    while not inputQ.empty():
+       
+        while not inputQ.empty():
+       
+            curr = inputQ.get()
+            print(curr.data, end=' ')
+            if curr.left!=None:
+                outputQ.put(curr.left)
+            if curr.right!=None:
+                outputQ.put(curr.right)
+       
+        print()
+        inputQ, outputQ = outputQ, inputQ
+
+
+# Main
+root = takeInput()
+
+pair = getMinAndMax(root)
+
+print(str(str(pair.minimum) + " " + str(pair.maximum)))
