e94da459583e1d9f40e4d57035dd8e16032154c6
sweta <52633654+sweta1609@users.noreply.github.com>
2022-03-12 20:54:09 +0530

    Create Diameter Of Binary Tree.py

---
+++ Diameter Of Binary Tree.py
@@ -0,0 +1,110 @@
+from sys import stdin, setrecursionlimit
+import queue
+
+setrecursionlimit(10 ** 6)
+
+
+#Following is the structure used to represent the Binary Tree Node
+class BinaryTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+def diameterOfBinaryTreeHelper(root):
+    if(root == None):
+        return (0, 0)
+    (leftheight, leftdiameter) = diameterOfBinaryTreeHelper(root.left)
+    (rightheight, rightdiameter) = diameterOfBinaryTreeHelper(root.right)
+    height = 1 + max(leftheight, rightheight)
+    diameter = max(1 + leftheight + rightheight, leftdiameter, rightdiameter)
+    return (height, diameter)
+   
+        
+def diameterOfBinaryTree(root): 
+    (height, diameter) = diameterOfBinaryTreeHelper(root)
+    return diameter
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Taking level-order input using fast I/O method
+def takeInput():
+    levelOrder = list(map(int, stdin.readline().strip().split(" ")))
+    start = 0
+    
+    length = len(levelOrder)
+
+    if length == 1 :
+        return None
+    
+    root = BinaryTreeNode(levelOrder[start])
+    start += 1
+
+    q = queue.Queue()
+    q.put(root)
+
+    while not q.empty():
+        currentNode = q.get()
+
+        leftChild = levelOrder[start]
+        start += 1
+
+        if leftChild != -1:
+            leftNode = BinaryTreeNode(leftChild)
+            currentNode.left =leftNode
+            q.put(leftNode)
+
+        rightChild = levelOrder[start]
+        start += 1
+
+        if rightChild != -1:
+            rightNode = BinaryTreeNode(rightChild)
+            currentNode.right =rightNode
+            q.put(rightNode)
+
+    return root
+
+    
+def printLevelWise(root):
+    if root==None:
+        return
+
+    inputQ = queue.Queue()
+    outputQ = queue.Queue()
+    inputQ.put(root)
+
+    while not inputQ.empty():
+       
+        while not inputQ.empty():
+       
+            curr = inputQ.get()
+            print(curr.data, end=' ')
+            if curr.left!=None:
+                outputQ.put(curr.left)
+            if curr.right!=None:
+                outputQ.put(curr.right)
+       
+        print()
+        inputQ, outputQ = outputQ, inputQ
+
+
+# Main
+root = takeInput()
+
+print(diameterOfBinaryTree(root))
